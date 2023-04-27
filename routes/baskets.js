e626d898e063a37cd249ce4b866622d3dbc362af
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-04-27 09:56:15 -0400

    Adds authentication and routes for items

--- routes/baskets.js
+++ routes/baskets.js
@@ -1,76 +1,73 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { authenticate } = require('../middlewares/auth');
+const { authenticate } = require("../middlewares/auth");
 
-const { Basket, BasketItem, Item } = require('../models');
+const { Basket, BasketItem, Item } = require("../models");
 
 // Create a new basket
-router.post('/', async (req, res) => {
+router.post("/", authenticate, async (req, res) => {
   try {
     const basket = await Basket.create(req.body);
     res.status(201).json(basket);
   } catch (error) {
-    res.status(500).json({ message: 'Error creating basket', error });
+    res.status(500).json({ message: "Error creating basket", error });
   }
 });
 
 // Get all baskets, including associated items
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const baskets = await Basket.findAll(); // how can we include the ITEMS associated with the baskets in this response?
-    res.json(baskets);
+    res.status(201).json(baskets);
   } catch (error) {
-    res.status(500).json({ message: 'Error retrieving baskets', error });
+    res.status(500).json({ message: "Error retrieving baskets", error });
   }
 });
 
 // Get a specific basket by ID, including associated items
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const basket = await Basket.findByPk(req.params.id); // how can we include the ITEMS associated with the baskets in this response?
-
     if (!basket) {
-      res.status(404).json({ message: 'Basket not found' });
+      res.status(404).json({ message: "Basket not found" });
     } else {
       res.json(basket);
     }
   } catch (error) {
-    res.status(500).json({ message: 'Error retrieving basket', error });
+    res.status(500).json({ message: "Error retrieving basket", error });
   }
 });
 
 // Update a basket by ID
-router.put('/:id', async (req, res) => {
+router.put("/:id", authenticate, async (req, res) => {
   try {
     const [updated] = await Basket.update(req.body, {
       where: { id: req.params.id },
     });
-
     if (updated) {
       const updatedBasket = await Basket.findByPk(req.params.id);
       res.json(updatedBasket);
     } else {
-      res.status(404).json({ message: 'Basket not found' });
+      res.status(404).json({ message: "Basket not found" });
     }
   } catch (error) {
-    res.status(500).json({ message: 'Error updating basket', error });
+    res.status(500).json({ message: "Error updating basket", error });
   }
 });
 
 // Delete a basket by ID
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", authenticate, async (req, res) => {
   try {
     const deleted = await Basket.destroy({
       where: { id: req.params.id },
     });
-
     if (deleted) {
-      res.status(204).json({ message: 'Basket deleted' });
+      res.status(204).json({ message: "Basket deleted" });
     } else {
-      res.status(404).json({ message: 'Basket not found' });
+      res.status(404).json({ message: "Basket not found" });
     }
   } catch (error) {
-    res.status(500).json({ message: 'Error deleting basket', error });
+    res.status(500).json({ message: "Error deleting basket", error });
   }
 });
 
