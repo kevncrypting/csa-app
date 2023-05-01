ff1ed55bb0bd948525e78dbd284f3e9e664be818
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-05-01 11:32:07 -0400

    Adds host to production config

--- config/config.js
+++ config/config.js
@@ -18,6 +18,7 @@ module.exports = {
   "production": {
     "username": process.env.DATABASE_USER,
     "password": process.env.DATABASE_PASSWORD,
-    "database": process.env.DATABASE_NAME
+    "database": process.env.DATABASE_NAME,
+    "host": process.env.DATABASE_HOST,
   }
 }
